f45f57401fed9297749670b293d8acd3e8567358
kalu6334 <martin.lucas76334@gmail.com>
2021-10-28 21:08:17 +0200

    finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+    doctor = Doctor.create!(
+        first_name:Faker::Name.first_name,
+        last_name:Faker::Name.last_name,
+        speciality: ["Chirurgien","Ophtalmo","Cardio","Dermato","ORL","Neurologue","Generaliste","Rhumatologue","Urologue","Pédiatre"].sample,
+        zip_code:Faker::Address.zip_code
+    )
+end 
+
+50.times do 
+    patient = Patient.create!(
+        first_name:Faker::Name.first_name,
+        last_name:Faker::Name.last_name,
+    ) 
+end 
+
+
+100.times do 
+    rendez_vous = Appointment.create!(doctor:Doctor.all.sample, patient:Patient.all.sample)
+end
+
+
+
+
